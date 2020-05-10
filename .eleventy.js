60a9894807f12ad424b0e7f865b41bc6ce85419e
Mike Schmidt <mschmidty@gmail.com>
2020-05-10 07:23:25 -0600

    made mobile ready

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,21 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     "md",
-    "css",
-    "html" // css is not yet a recognized template extension in Eleventy
-  ]);
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("data");
-  eleventyConfig.addPassthroughCopy("bird_songs_of_the_rockies");
-  eleventyConfig.addPassthroughCopy("images");
+    "html", 
+    "css" // css is not yet a recognized template extension in Eleventy
+  ])
+  eleventyConfig.addPassthroughCopy([
+    "js",
+    "data",
+    "bird_songs_of_the_rockies",
+    "images"
+  ])
+  // eleventyConfig.addPassthroughCopy("js");
+  // eleventyConfig.addPassthroughCopy("data");
+  // eleventyConfig.addPassthroughCopy("bird_songs_of_the_rockies");
+  // eleventyConfig.addPassthroughCopy("images");
+  // eleventyConfig.addPassthroughCopy("css");
   return{
-    pathPrefix:'bird_id_trainer'
-  };
+    pathPrefix:"/bird_id_trainer/",
+  }
 };
